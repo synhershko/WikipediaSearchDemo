3fce0e1e232193d14afd3cfef9db2f47d66e681b
Itamar Syn-Hershko <itamar@code972.com>
2013-09-17 18:34:18 +0200

    Adding faceting support on categories

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,25 +7,44 @@ angular.module('WikipediaSearchApp')
         var oQuery = ejs.QueryStringQuery().fields(['title^10', 'text'])
             .analyzer('wiki_analyzer')
             .useDisMax(false).defaultOperator('AND');
-        var oFilter = ejs.AndFilter(
+        var oFilters =
             [
                 ejs.MissingFilter('redirect'),
                 ejs.MissingFilter('special'),
                 ejs.MissingFilter('stub'),
                 ejs.MissingFilter('disambiguation')
-            ]
-        );
+            ];
 
-        var client = ejs.Request()
-            .indices('wiki-enwiki')
-            .types('wikipage');
-
+        // Paging stuff
         $scope.currentPage = 1;
         $scope.pageSize = 20;
-
         $scope.numberOfPages = function(){
             return Math.floor($scope.totalResults/$scope.pageSize);
         };
+
+        // Category filtering support
+        var activeFilters = {};
+        $scope.isActive = function (field, term) {
+            return activeFilters.hasOwnProperty(field + term);
+        };
+        $scope.filter = function(field, term) {
+            if ($scope.isActive(field, term)) {
+                delete activeFilters[field + term];
+            } else {
+                activeFilters[field + term] = ejs.TermFilter(field, term);
+            }
+            $scope.search();
+        };
+        var getFilters = function() {
+            var filters = Object.keys(activeFilters).map(function(k) { return activeFilters[k]; });
+
+            return filters.length > 0 ? ejs.AndFilter(oFilters.concat(filters)) : ejs.AndFilter(oFilters);
+        };
+
+        // Where the actual search happens (decleration of it at least)
+        var client = ejs.Request()
+            .indices('wiki-enwiki')
+            .types('wikipage');
 
         $scope.search = function(page) {
             if (page == null)
@@ -34,10 +53,12 @@ angular.module('WikipediaSearchApp')
                 $scope.currentPage = page;
 
             $scope.results = client
-                .query(oQuery.query($scope.queryTerm || '*'))
-                .filter(oFilter)
+                .query(ejs.FilteredQuery(oQuery.query($scope.queryTerm || '*'), getFilters()))
                 .highlight(ejs.Highlight(['title', 'text']).type('fast-vector-highlighter'))
                 .from(($scope.currentPage - 1) * $scope.pageSize).size($scope.pageSize)
+
+                .facet(ejs.TermsFacet('categories').field('category').size(20).facetFilter(getFilters()))
+
                 .doSearch(function(rsp) { $scope.totalResults = rsp.hits.total; });
         };
   });
